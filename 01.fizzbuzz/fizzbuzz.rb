6fa2fe9e52ff6ceb27196d776a4417c8d43727aa
tomoka <tomoka.saiwaki@gmail.com>
2024-05-08 07:50:53 +0900

    Change from <while> to <each>method

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,14 +1,11 @@
-num = 1
-
-while num <= 20
-    if num % 3 == 0 && num % 5 == 0
-        puts "FizzBuzz"
-    elsif num % 3 == 0
-        puts "Fizz"
-    elsif num % 5 == 0
-        puts "Buzz"
-    else
-        puts num
-    end
-    num += 1
+(1..20).each do |num|
+  if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  else
+    puts num
+  end
 end
