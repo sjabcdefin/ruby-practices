d3ab33c48e20c802b5dd329151e38c43c25d4fae
tomoka <tomoka.saiwaki@gmail.com>
2024-05-11 16:23:26 +0900

    Correct the indicated items

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -5,39 +5,32 @@ require 'optparse'
 require 'date'
 
 class Calendar
-  def set_arg
-    # コマンドライン引数から年、月の値を取得する
+  def initialize
     opt = OptionParser.new
     opt.on('-y [YEAR]') { |v| @year = v.to_i }
     opt.on('-m [MONTH]') { |v| @month = v.to_i }
     opt.parse!(ARGV)
 
-    # 引数指定がなかった場合、現在年月を設定する
     today = Date.today
-    @year = today.year if @year.nil?
-    return unless @month.nil?
-
-    @month = today.month
+    @year ||= today.year
+    @month ||= today.month
   end
 
   def display_title
-    # 年月表示
-    year_month = @month.to_s + '月 ' + @year.to_s
+    year_month = "#{@month}月 #{@year}"
     puts year_month.center(20)
 
-    # 曜日表示
     weeks = %w[日 月 火 水 木 金 土]
     puts weeks.join(' ')
   end
 
   def display_calendar
-    lastday = Date.new(@year, @month, -1)
-    day = 1
-    distance = 2
+    lastdate = Date.new(@year, @month, -1)
 
-    while day <= lastday.day
-      date = Date.new(@year, @month, day)
-      if day == 1
+    (1..lastdate.day).each do |num|
+      date = Date.new(@year, @month, num)
+      distance = 2
+      if num == 1
         distance += date.wday * 3
       else
         distance = if date.wday.zero?
@@ -47,16 +40,14 @@ class Calendar
                    end
       end
       if date.wday == 6
-        puts day.to_s.rjust(distance)
+        puts num.to_s.rjust(distance)
       else
-        print day.to_s.rjust(distance)
+        print num.to_s.rjust(distance)
       end
-      day += 1
     end
   end
 end
 
-x = Calendar.new
-x.set_arg
-x.display_title
-x.display_calendar
+calendar = Calendar.new
+calendar.display_title
+calendar.display_calendar
