0a90497e2152639bc2cf5a7ff230e29564050706
tomoka <tomoka.saiwaki@gmail.com>
2024-06-16 16:37:10 +0900

    Add -a option

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,11 +1,26 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+
 COLUMN_SIZE = 3
 COLUMN_SPACE = 2
 
-def find_file_information
+def set_command_line_option
+  command_line_option = nil
+  opts = OptionParser.new
+  opts.on('-a') { command_line_option = 'a' }
+  opts.parse!(ARGV)
+  specify_command_line_option(command_line_option)
+end
+
+def specify_command_line_option(command_line_option)
   files = Dir.glob('*')
+  files = Dir.entries('.').sort! if command_line_option
+  find_file_information(files)
+end
+
+def find_file_information(files)
   interval = files.size.ceildiv(COLUMN_SIZE)
   max_file_size = files.map(&:length).max
   space_size = max_file_size + COLUMN_SPACE
@@ -24,4 +39,4 @@ def display_file(files, interval, space_size)
   end
 end
 
-find_file_information
+set_command_line_option
