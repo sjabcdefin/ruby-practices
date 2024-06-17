4d2d3324602a10bad8ffd84198b1109c39f7eca2
tomoka <tomoka.saiwaki@gmail.com>
2024-06-17 19:14:36 +0900

    Correct the indicated items

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -6,28 +6,34 @@ require 'optparse'
 COLUMN_SIZE = 3
 COLUMN_SPACE = 2
 
-def set_command_line_option
+def configure_command_line_option
   command_line_option = nil
   opts = OptionParser.new
   opts.on('-a') { command_line_option = 'a' }
   opts.parse!(ARGV)
-  specify_command_line_option(command_line_option)
+  command_line_option
 end
 
-def specify_command_line_option(command_line_option)
-  files = Dir.glob('*')
-  files = Dir.entries('.').sort! if command_line_option
-  find_file_information(files)
+def configure_file_name_by_command_line_option(command_line_option)
+  if command_line_option
+    Dir.glob('*', File::FNM_DOTMATCH).sort!
+  else
+    Dir.glob('*')
+  end
 end
 
-def find_file_information(files)
+def configure_file_information(files)
   interval = files.size.ceildiv(COLUMN_SIZE)
   max_file_size = files.map(&:length).max
   space_size = max_file_size + COLUMN_SPACE
-  display_file(files, interval, space_size)
+  [interval, space_size]
 end
 
-def display_file(files, interval, space_size)
+def display_file_and_directory_in_3_columns
+  command_line_option = configure_command_line_option
+  files = configure_file_name_by_command_line_option(command_line_option)
+  interval, space_size = configure_file_information(files)
+
   (0...interval).each do |num|
     file_array = []
     line = num
@@ -39,4 +45,4 @@ def display_file(files, interval, space_size)
   end
 end
 
-set_command_line_option
+display_file_and_directory_in_3_columns
