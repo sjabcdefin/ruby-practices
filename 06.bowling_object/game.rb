440d0aaaf83778b7c56f0b2219a57364ade30994
tomoka <tomoka.saiwaki@gmail.com>
2024-12-04 18:22:06 +0900

    Update: Change variable name and method name
    変数名とメソッド名を変更

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -5,50 +5,50 @@ require_relative './frame'
 
 class Game
   def initialize
-    @frames = parse_input
+    @frames = split_into_frames
   end
 
-  def parse_input
+  def split_into_frames
     score = ARGV[0]
 
     scores = score.split(',')
 
-    @frames = []
+    frames = []
     frame = []
 
     scores.each do |s|
       frame << s
 
-      if @frames.size < 10
+      if frames.size < 10
         if frame.size >= 2 || s == 'X'
-          @frames << frame.dup
+          frames << frame.dup
           frame.clear
         end
       else
-        @frames.last << s
+        frames.last << s
       end
     end
-    @frames
+    frames
   end
 
-  def points
+  def point
     point = 0
     (0..9).each do |n|
-      frame, next_frame, after_next_frame = @frames.slice(n, 3)
-      next_frame ||= []
-      after_next_frame ||= []
+      frame_data, next_frame_data, after_next_frame_data = @frames.slice(n, 3)
+      next_frame_data ||= []
+      after_next_frame_data ||= []
 
-      frame_t = Frame.new(frame[0], frame[1], frame[2])
-      next_frame_t = Frame.new(next_frame[0], next_frame[1], next_frame[2])
-      after_next_frame_t = Frame.new(after_next_frame[0], after_next_frame[1], after_next_frame[2])
+      frame = Frame.new(frame_data[0], frame_data[1], frame_data[2])
+      next_frame = Frame.new(next_frame_data[0], next_frame_data[1], next_frame_data[2])
+      after_next_frame = Frame.new(after_next_frame_data[0], after_next_frame_data[1], after_next_frame_data[2])
 
-      if frame_t.strike?
-        point += frame_t.score + next_frame_t.first_shot.score + next_frame_t.second_shot.score
-        point += after_next_frame_t.first_shot.score if next_frame_t.first_shot.score == 10
-      elsif frame_t.spare?
-        point += frame_t.score + next_frame_t.first_shot.score
+      if frame.strike?
+        point += frame.score + next_frame.first_shot.score + next_frame.second_shot.score
+        point += after_next_frame.first_shot.score if next_frame.first_shot.score == 10
+      elsif frame.spare?
+        point += frame.score + next_frame.first_shot.score
       else
-        point += frame_t.score
+        point += frame.score
       end
     end
     point
@@ -56,4 +56,4 @@ class Game
 end
 
 game = Game.new
-puts game.points
+puts game.point
